b37c118312e50f2a7f0dad3c5289e7dc6eb7f460
Apostle1327 <apostle1327@gmail.com>
2025-01-11 16:25:20 +0530

    Lint modifications

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,54 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
-    settings: { react: { version: '18.3' } },
+    settings: { react: { version: "18.3" } },
     plugins: {
       react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "no-console": "warn",
+      "no-restricted-globals": ["error", "event", "fdescribe"],
+      "consistent-return": "warn",
+      "no-unused-vars": "warn",
+      "prefer-const": "warn",
+      "no-restricted-syntax": [
+        "warn",
+        {
+          selector: 'CallExpression[callee.object.name="localStorage"]',
+          message:
+            "Direct use of localStorage detected. Consider adding checks and validations.",
+        },
+      ],
+      "no-try-catch": "off",
+      "react/jsx-uses-react": "off",
+      "react/jsx-uses-vars": "warn",
     },
   },
-]
+];
